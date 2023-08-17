e52d4819decb5b6f6735d7ce590a4abd3b613043
replayzor <oltean.ionut1989@gmail.com>
2023-08-17 19:43:44 +0300

    add react query and axios

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
