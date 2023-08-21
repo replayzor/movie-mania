b560ac6807b26617aa134bb7886f9b3cec9bfcad
replayzor <oltean.ionut1989@gmail.com>
2023-08-21 19:39:21 +0300

    add loading and error

--- src/components/Movies/MoviesList.tsx
+++ src/components/Movies/MoviesList.tsx
@@ -1,11 +1,36 @@
 import { MovieTypes } from "../../types/movieTypes";
+import Loading from "../Loading";
 import Movie from "./Movie";
 
 type MovieListProps = {
 	movies: MovieTypes[];
+	isLoading: boolean;
+	isError: boolean;
 };
 
-const MoviesList = ({ movies }: MovieListProps) => {
+const MoviesList = ({ movies, isLoading, isError }: MovieListProps) => {
+	if (isLoading) {
+		return <Loading />;
+	}
+
+	if (movies.length === 0) {
+		return (
+			<div className="error-container">
+				<h1>No movies found</h1>
+				<p>Sorry, we could not find any movies with that name.</p>
+			</div>
+		);
+	}
+
+	if (isError) {
+		return (
+			<div className="error-container">
+				<h1>Error loading movies</h1>
+				<p>Sorry, we encountered an error while loading movies.</p>
+			</div>
+		);
+	}
+
 	return (
 		<ul className="list">
 			{movies?.map((movie) => (
