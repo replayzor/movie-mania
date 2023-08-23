50b80e06a20316c453c8183e1926db8efd2e7c10
replayzor <oltean.ionut1989@gmail.com>
2023-08-23 16:33:54 +0300

    Add functionality for no movies and search for movies

--- src/components/Movies/MoviesList.tsx
+++ src/components/Movies/MoviesList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { MovieTypes } from "../../types/movieTypes";
 import Loading from "../Loading";
 import Movie from "./Movie";
@@ -6,20 +7,25 @@ type MovieListProps = {
 	movies: MovieTypes[];
 	isLoading: boolean;
 	isError: boolean;
+	onSelectMovie: (id: string) => void;
 };
 
-const MoviesList = ({ movies, isLoading, isError }: MovieListProps) => {
+const MoviesList = ({
+	movies,
+	isLoading,
+	isError,
+	onSelectMovie,
+}: MovieListProps) => {
+	const [hasSearched, setHasSearched] = useState(false);
+
+	useEffect(() => {
+		if (movies.length > 0) {
+			setHasSearched(true);
+		}
+	}, [movies]);
+
 	if (isLoading) {
 		return <Loading />;
-	}
-
-	if (!movies.length) {
-		return (
-			<div className="error-container">
-				<h1>No movies found</h1>
-				<p>Sorry, we could not find any movies with that name.</p>
-			</div>
-		);
 	}
 
 	if (isError) {
@@ -31,10 +37,27 @@ const MoviesList = ({ movies, isLoading, isError }: MovieListProps) => {
 		);
 	}
 
+	if (!hasSearched) {
+		return (
+			<div className="error-container">
+				<h1>Start by searching for a movie.</h1>
+			</div>
+		);
+	}
+
+	if (movies.length === 0) {
+		return (
+			<div className="error-container">
+				<h1>No movies found</h1>
+				<p>Sorry, we could not find any movies with that name.</p>
+			</div>
+		);
+	}
+
 	return (
-		<ul className="list">
+		<ul className="list list-movies">
 			{movies?.map((movie) => (
-				<Movie key={movie.imdbID} movie={movie} />
+				<Movie onSelectMovie={onSelectMovie} key={movie.imdbID} movie={movie} />
 			))}
 		</ul>
 	);
