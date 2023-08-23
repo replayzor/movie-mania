4857cfea9ebd176fbc906d0233d689ea22ed82be
replayzor <oltean.ionut1989@gmail.com>
2023-08-23 23:14:49 +0300

    feat: Enhance StarRating component with hover messages and customization options
    - Integrate hover messages to provide contextual information for each star
    - Add customization options for maxRating, color, size, className, and messages
    - Improve component's default behavior with defaultRating option

---
+++ src/components/StarRating.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import Star from "./Star";
+
+const containerStyle = {
+	display: "flex",
+	alignItems: "center",
+	gap: "16px",
+};
+
+const starContainerStyle = {
+	display: "flex",
+};
+
+type StarRatingProps = {
+	maxRating?: number;
+	color?: string;
+	size?: number;
+	className?: string;
+	messages?: string[];
+	defaultRating?: number;
+};
+
+function StarRating({
+	maxRating = 5,
+	color = "#fcc419",
+	size = 48,
+	className = "",
+	messages = [],
+	defaultRating = 0,
+}: StarRatingProps) {
+	const [rating, setRating] = useState(defaultRating);
+	const [tempRating, setTempRating] = useState(0);
+
+	function handleRating(rating: number) {
+		setRating(rating);
+	}
+
+	const textStyle = {
+		lineHeight: "1",
+		margin: "0",
+		color,
+		fontSize: `${size / 1.5}px`,
+	};
+
+	return (
+		<div style={containerStyle} className={className}>
+			<div style={starContainerStyle}>
+				{Array.from({ length: maxRating }, (_, i) => (
+					<Star
+						key={i}
+						full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+						onRate={() => handleRating(i + 1)}
+						onHoverIn={() => setTempRating(i + 1)}
+						onHoverOut={() => setTempRating(0)}
+						color={color}
+						size={size}
+					/>
+				))}
+			</div>
+			<p style={textStyle}>
+				{messages.length === maxRating
+					? messages[tempRating ? tempRating - 1 : rating - 1]
+					: tempRating || rating || ""}
+			</p>
+		</div>
+	);
+}
+
+export default StarRating;
