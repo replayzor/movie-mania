4e86fe9916915e08aceaa6ab8c85e50e5135538f
replayzor <oltean.ionut1989@gmail.com>
2023-08-29 18:44:07 +0300

    feat: Enhance search input behavior and handling
    - Added auto-focus to the search input when the component mounts.
    - Implemented handling of the Enter key press to clear the search input.
    - Updated variable names for clarity and consistency.

--- src/components/Navbar/Search.tsx
+++ src/components/Navbar/Search.tsx
@@ -6,21 +6,35 @@ type SearchProps = {
 };
 
 const Search = ({ query, setQuery }: SearchProps) => {
-	const inputRef = useRef<HTMLInputElement>(null);
+	const inputEl = useRef<HTMLInputElement>(null);
 
 	useEffect(() => {
-		inputRef.current?.focus();
+		inputEl.current?.focus();
+
+		const handleKeyDown = (e: KeyboardEvent) => {
+			if (document.activeElement === inputEl.current) return;
+
+			if (e.code === "Enter") {
+				inputEl.current?.focus();
+				setQuery("");
+			}
+		};
+
+		document.addEventListener("keydown", handleKeyDown);
+		return () => {
+			document.addEventListener("keydown", handleKeyDown);
+		};
 	}, []);
 
 	return (
 		<div>
 			<input
-				ref={inputRef}
 				className="search"
 				type="text"
 				placeholder="Search movies..."
 				value={query}
 				onChange={(e) => setQuery(e.target.value)}
+				ref={inputEl}
 			/>
 		</div>
 	);
